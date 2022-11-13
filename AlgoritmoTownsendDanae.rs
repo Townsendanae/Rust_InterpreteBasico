c64cbdc2509b3083fbc4b6410b2a60492564f681
Danae Townsend <danjtown@espol.edu.ec>
2022-11-13 17:22:08 -0500

    Algoritmo Danaé Townsend

---
+++ AlgoritmoTownsendDanae.rs
@@ -0,0 +1,24 @@
+fn main() {
+    let number = 5;
+
+    if number < 0 {
+        print!("{} - numero es negativo", number;
+    } else if number > 0 {
+        print!("{} - numero es positivo", number);
+    } else {
+        print!("{} - numero es cero", number);
+    }
+
+    while n < 10 && n > -10 {
+        if number % 2 == 0 {
+            println!(":)");
+        }else {
+            println!(":D", number);
+        }
+        number += 1;
+    }
+
+    for number2 in 1..3 {    
+            println!("{} - ", number2); 
+    }
+}
