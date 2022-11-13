7348881beb033f9578fcbf773104518daad265e7
Danae Townsend <danjtown@espol.edu.ec>
2022-11-13 16:44:26 -0500

    Expresiones regulares simples y expresión regular regla para variables

--- AnalizadorLexico.py
+++ AnalizadorLexico.py
@@ -1,7 +1,6 @@
 import ply.lex as lex
 
-#Palabras reservadas
-
+#reserverd words
 reserved = {
   'as':'AS',
   'use':'USE',
@@ -13,38 +12,88 @@ reserved = {
   'else':'ELSE',
   'if':'IF',
   'if let':'IF LET'
-  #completar
+  # --> completar Ronald
 
 }
 
 #definir listado de tokens
-signs_tokens = [
-    'PLUS','MINUS','TIMES','DIVIDE','MATH_REMINDER','ASSIGN','EQUALS','NOT_EQUALS','LESS_THAN','LESSEQUAL_THAN',
-    'MORE_THAN','MOREEQUAL_THAN','AND','OR','NOT','AND_BIT','OR_BIT','XOR_BIT','NOT_BIT','LEFT_MAYUS','RIGHT_MAYUS',
-    'LPAREN','RPAREN','VARIABLE'
-]
-
-data_type_tokens = [
-    'INTEGER','DECIMAL','HEXADECIMAL' ##completar
+punctuation_tokens = [
+    'LPAREN','RPAREN'
     ]
 
-tokens = signs_tokens + data_type_tokens + list(reserved.values())
+math_tokens = [
+    'PLUS','MINUS','TIMES','DIVIDE','MATH_REMINDER'
+]
 
-#Expresiones regulares
+compare_tokens = [
+    'ASSIGN','EQUALS','NOT_EQUALS','LESS_THAN','LESSEQUAL_THAN',
+    'MORE_THAN','MOREEQUAL_THAN'
+    ]
+
+logic_tokens = [
+    'AND','OR','NOT'
+    ]
+
+bit_tokens = [
+    'AND_BIT','OR_BIT','XOR_BIT','LEFT_MAYUS','RIGHT_MAYUS'
+    ]
+
+
+data_type_tokens = [
+    'VARIABLE','INTEGER','DECIMAL','HEXADECIMAL' ## -->completar David
+    ]
+
+tokens = punctuation_tokens + math_tokens + compare_tokens + logic_tokens + bit_tokens + data_type_tokens + list(reserved.values())
+
+
+#Expresiones regulares - punctuation
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+
+#Expresiones regulares - math
 t_PLUS = r'\+'
-t_PLUS = r'\+'
-t_PLUS = r'\+'
-t_PLUS = r'\+'
-t_PLUS = r'\+'
-t_PLUS = r'\+'
-t_PLUS = r'\+'
-t_PLUS = r'\+'
-t_PLUS = r'\+'
-t_PLUS = r'\+'
-t_PLUS = r'\+'
-t_PLUS = r'\+'
-t_PLUS = r'\+'
-t_PLUS = r'\+'
-t_PLUS = r'\+'
+t_MINUS = r'-'
+t_TIMES = r'\*'
+t_DIVIDE = r'/'
+t_MATH_REMINDER = r'%'
+
+#Expresiones regulares - compare
+t_ASSIGN = r'='
+t_EQUALS = r'=='
+t_NOT_EQUALS = r'!='
+t_LESS_THAN = r'<'
+t_LESSEQUAL_THAN = r'<='
+t_MORE_THAN = r'>'
+t_MOREEQUAL_THAN = r'=>'
+
+#Expresiones regulares - logic
+t_AND = r'&&'
+t_OR = r'\|\|'
+t_NOT = r'!'
+
+#Expresiones regulares - bit
+t_AND_BIT = r'&'
+t_OR_BIT = r'\|'
+t_XOR_BIT = r'^'
+t_LEFT_MAYUS = r'<<'
+t_RIGHT_MAYUS = r'>>'
+
+#Expresiones regulares con reglas - tipo de datos --> Completar David y Ronald mitad y mitad
+
+def t_VARIABLE(t):
+    r'[a-zA-Z_][a-zA-Z0-9]*'
+    t.type = reserved.get(t.value,'VARIABLE')
+    return t
+
+def t_INTEGER(t):
+    pass
+
+def t_DECIMAL(t):
+    pass
+
+def t_HEXADECIMAL(t):
+    pass
+
+
 
 
