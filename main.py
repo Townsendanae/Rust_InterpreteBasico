e0ac266673d98df212f7ea746a4210f6a05a7f62
Danae Townsend <danjtown@espol.edu.ec>
2022-11-29 19:23:58 -0500

    Mejoras en la interfaz

--- main.py
+++ main.py
@@ -3,21 +3,50 @@ import tkinter as tk
 from tkinter.ttk import *
 from AnalizadorLexico import *
 from AnalizadorSintactico import *
+from tkinter import filedialog
 
+#funciones extras
+def open_file():
+    archivo_Ruta = filedialog.askopenfilename(
+        initialdir = "/", title = "Seleccione archivo", 
+        filetypes = (("Rust files",".rs"), ("all files",".*")))
+    print(archivo_Ruta)
+    archivo = open(archivo_Ruta)
+    content = ""
+    for line in archivo: 
+        content += line
+    print(content)
+    entrada.set(content)
+
+def popup(lst):
+    panel2 = Tk()
+    ventana2 = Frame(panel2,width=500,height=500)
+    ventana2.pack()
+    scrollbar = Scrollbar(ventana2)
+    scrollbar.pack( side = RIGHT, fill = BOTH )
+    mylist = Listbox(ventana2, yscrollcommand = scrollbar.set , width=100)
+    mylist.place(x = 10,
+        y = 10,
+        width=250,
+        height=150)
+    mylist.pack()
+    scrollbar.config( command = mylist.yview )
+    for line in lst:
+        mylist.insert(END, line + '\n')
 
 #Funciones analisis lexico
 def show_analisis_lexico():
     content=entrada.get()
     lexer.input(content)
     getTokens(lexer)
-    string_output.set("\n".join(lstTokens))  
+    popup(lstTokens)  
 
 
 #Funciones analisis sintáctico
 def show_analysis_sintactico(): #comentar el while True del analizador sintactico y colocar el clear a la lista en validarRegla
     s = entrada.get()
-    validaRegla(s)
-    string_output.set("\n".join(lstErrores))  
+    validaRegla(s)  
+    popup(lstErrores)
 
 #funciones analisis semantico
 def show_analysis_semantico():
@@ -25,21 +54,21 @@ def show_analysis_semantico():
     print(content)
 
 
-
-
 #main
 panel = Tk()
-ventana = Frame(panel,width=500,height=500)
+ventana = Frame(panel,width=500,height=300)
 ventana.pack()
-titulo = tk.Label(text="ANALIZADOR BÁSICO DE RUST",font=("Arial",18), fg="black", bg="aliceblue").place(x=5,y=8)
-lb1 = tk.Label( text="Ingrese código en Rust",fg="black", bg="ivory").place(x=10,y=50)
+
+
+titulo = tk.Label(ventana,text="ANALIZADOR BÁSICO DE RUST",font=("Arial",18), fg="black", bg="aliceblue").place(x=5,y=8)
+lb1 = tk.Label( ventana,text="Ingrese código en Rust",fg="black", bg="ivory").place(x=10,y=50)
 entrada = tk.StringVar()
-tk.Entry(textvariable=entrada, font=("Arial",8), width=80).place(height=60,x=10,y=80)
-botonLexico = tk.Button(text="Analizador Léxico",  command=show_analisis_lexico).place(width=130,x=10,y=150)
-botonSintactico = tk.Button(text="Analizador Sintáctico",  command=show_analysis_sintactico).place(width=130,x=160,y=150)
-botonSemantico = tk.Button(text="Analizador Semántico",  command=show_analysis_semantico).place(width=130,x=310,y=150)
-lb2_Title = tk.Label( text="Salida: ",fg="black", bg="ivory").place(x=10,y=200)
-string_output = StringVar()
-string_output.set("")
-lb3_Output = tk.Label(textvariable=string_output,fg="black", bg="ivory").place(x=10,y=230)
+tk.Entry(ventana,textvariable=entrada, font=("Arial",8), width=80).place(height=60,x=10,y=80)
+botonLexico = tk.Button(ventana,text="Analizador Léxico",  command=show_analisis_lexico).place(width=130,x=10,y=150)
+botonSintactico = tk.Button(ventana,text="Analizador Sintáctico",  command=show_analysis_sintactico).place(width=130,x=160,y=150)
+botonSemantico = tk.Button(ventana,text="Analizador Semántico",  command=show_analysis_semantico).place(width=130,x=310,y=150)
+botonOpenFile = tk.Button(ventana,text="Abrir archivo",  command=open_file).place(width=130,x=160,y=200)
+lb2_Title = tk.Label(ventana, text="Salida: ",fg="black", bg="ivory").place(x=10,y=250)
+
+
 ventana.mainloop()
