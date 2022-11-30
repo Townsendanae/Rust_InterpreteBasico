3d1455258219f4325f2033db77e0af16c8c488a2
rxcp02 <rxcabrer@espol.edu.ec>
2022-11-29 23:14:34 -0500

    Cambios Ronald

--- AnalizadorSintactico.py
+++ AnalizadorSintactico.py
@@ -1,80 +1,28 @@
 import ply.yacc as yacc
 from datetime import datetime
 from AnalizadorLexico import tokens
-"""
-# Rommel
 
-# Rommel
-
-
-
-# Estructuras de control
-#match: Rommel
-
-
-
-def p_imprimir(p):
-    'imprimir : PRINTLN NOT LPAREN valor RPAREN'
-
-#loop: Ronald
-
-
-# for: Danae
-
-
-# Estructuras de datos
-#vectores: Danae
-
-
-
-#structs: Ronald
-
-
-
-# Reglas
-# definicion de funciones y mutabilidad: Rommel 
-
-
-# casting, alias y enlaces a variables: Ronald
-
-def p_casting(p):
-    '''casting : valor AS valor
-    | valor AS casting
-    | VARIABLE AS valor
-    | VARIABLE AS casting'''
-
-
-def p_alias(p):
-    'alias : TYPE VARIABLE ASSIGN valor COMMA_DOT'
-
-
-def p_link(p):
-    'link : LET LPAREN variables RPAREN ASSIGN LPAREN valores RPAREN COMMA_DOT'
-
-
-def p_variables(p):
-    '''variables : VARIABLE 
-    | VARIABLE COMA variables'''
-
-# apuntadores, traits y parametros:Danae
-
-
-# Por hacer: entrada y salida de datos
-
-"""
 
 def p_cuerpo(p):
-    '''cuerpo : asignacion
+    '''cuerpo : linea
+    | linea cuerpo
+    '''
+
+
+def p_linea(p):
+    '''linea : asignacion
     | struct
     | varEnVar
     | definirFuncion
     | hashMap
     | vector
     | for 
-    | for cuerpo
     | loop
     | eMatch
-    '''    
+    | casting
+    | alias
+    '''
+
 
 def p_asignacion(p):
     '''asignacion : definicion ASSIGN valorAsignado COMMA_DOT
@@ -82,18 +30,36 @@ def p_asignacion(p):
     | VARIABLE ASSIGN varEnVar COMMA_DOT
     | definicion'''
 
+
 def p_varEnVar(p):
     'varEnVar : VARIABLE POINT VARIABLE'
+
 
 def p_definicion(p):
     '''definicion : creacion
     | creacion COMMA_DOT
     | creacion tipoDato'''
 
+
 def p_creacion(p):
-    '''creacion : LET MUT VARIABLE
+    '''creacion : LET MUT variables
+    | LET variables
+    | CONST variables
+    | LET MUT VARIABLE
     | LET VARIABLE
     | CONST VARIABLE'''
+
+# enlaces: Ronald
+
+
+def p_variables(p):
+    '''variables : LPAREN conjuntoVariables RPAREN'''
+
+
+def p_conjuntoVariables(p):
+    '''conjuntoVariables : VARIABLE COMA VARIABLE
+    | VARIABLE COMA conjuntoVariables'''
+
 
 def p_tipoDato(p):
     '''tipoDato : TWO_POINTS dato
@@ -101,6 +67,7 @@ def p_tipoDato(p):
     | dato LESS_THAN dato MORE_THAN
     | TWO_POINTS apuntador
     '''
+
 
 def p_dato(p):
     '''dato : STRING
@@ -115,16 +82,25 @@ def p_dato(p):
     | GENERIC
     '''
 
+
 def p_valorAsignado(p):
     '''valorAsignado : dato
     | llamadaAfuncion
     | llamadaAfuncionPorAlias
     | vector
     | hashMap
+    | LPAREN conjuntoDatos RPAREN
     | VARIABLE'''
+
+
+def p_conjuntoDatos(p):
+    '''conjuntoDatos : dato COMA dato
+    | dato COMA conjuntoDatos'''
+
 
 def p_llamadaAfuncionPorAlias(p):
     '''llamadaAfuncionPorAlias : VARIABLE POINT llamadaAfuncion'''
+
 
 def p_llamadaAfuncion(p):
     '''llamadaAfuncion : VARIABLE argumentos
@@ -133,9 +109,11 @@ def p_llamadaAfuncion(p):
     | VARIABLE TWO_POINTS TWO_POINTS
     | VARIABLE TWO_POINTS TWO_POINTS llamadaAfuncion'''
 
+
 def p_argumentos(p):
     '''argumentos : LPAREN argumento RPAREN
     | LPAREN RPAREN'''
+
 
 def p_argumento(p):
     '''argumento : VARIABLE
@@ -147,19 +125,23 @@ def p_argumento(p):
     | llamadaAfuncion COMA argumento
     | llamadaAfuncionPorAlias COMA argumento'''
 
+
 def p_definirFuncion(p):
     ''' definirFuncion : FN VARIABLE trait parametros LKEY cuerpoFuncion RKEY
     | FN VARIABLE parametros LKEY cuerpoFuncion RKEY
     '''
 
+
 def p_trait(p):
     'trait : LESS_THAN GENERIC TWO_POINTS VARIABLE MORE_THAN '
+
 
 def p_datoAretornar(p):
     ''' datoARetornar : MINUS MORE_THAN dato
     | MINUS MORE_THAN NOT
     | MINUS MORE_THAN tipoDato
     '''
+
 
 def p_parametros(p):
     '''parametros : LPAREN RPAREN datoARetornar
@@ -175,33 +157,44 @@ def p_parametro(p):  # Corrección por Danae
     | dato
     '''
 
+
 def p_cuerpoFuncion(p):
     ''' cuerpoFuncion : cuerpo
     '''
 
 #structs: Ronald
+
+
 def p_struct(p):
     'struct : STRUCT VARIABLE LKEY atributos RKEY'
+
 
 def p_atributos(p):
     '''atributos : VARIABLE TWO_POINTS dato
     | VARIABLE TWO_POINTS dato COMA atributos'''
 
-#vectores: Danae, agregar la otra forma de definir vectores
+# vectores: Danae, agregar la otra forma de definir vectores
+
+
 def p_vector(p):
     '''vector : VEC NOT LBRACKET datos RBRACKET 
     | VEC TWO_POINTS TWO_POINTS NEW LPAREN RPAREN
     | VEC TWO_POINTS TWO_POINTS NEW LPAREN datos RPAREN'''
 
+
 def p_datos(p):
     '''datos : dato
-    | dato COMA datos''' 
+    | dato COMA datos'''
 
 #hashmap: Rommel
+
+
 def p_hashMap(p):
     'hashMap : HASHMAP TWO_POINTS TWO_POINTS NEW LPAREN RPAREN'
 
 # for: Danae
+
+
 def p_for(p):
     'for : FOR VARIABLE IN rango LKEY cuerpoFuncion RKEY'
 
@@ -209,8 +202,10 @@ def p_for(p):
 def p_rango(p):
     ''' rango : DECIMAL POINT POINT DECIMAL
     | DECIMAL POINT POINT ASSIGN DECIMAL '''
-    
+
 #match: Rommel
+
+
 def p_eMatch(p):
     'eMatch : MATCH VARIABLE LKEY content RKEY'
 
@@ -220,15 +215,32 @@ def p_content(p):
     | dato MOREEQUAL_THAN cuerpoFuncion COMA content'''
 
 #loop: Ronald
+
+
 def p_loop(p):
     'loop : LOOP LKEY cuerpoFuncion RKEY'
+
 
 def p_apuntador(p):
     'apuntador : FN parametros'
 
+# casting, alias: Ronald
+
+
+def p_casting(p):
+    '''casting : dato AS dato
+    | dato AS casting
+    | VARIABLE AS dato
+    | VARIABLE AS casting'''
+
+
+def p_alias(p):
+    'alias : TYPE VARIABLE ASSIGN dato COMMA_DOT'
+
+
 def p_error(p):
     if p:
-        msg = f"Error de sintaxis - Token: {p.type}, Línea: {p.lineno}, Col: {p.lexpos}" 
+        msg = f"Error de sintaxis - Token: {p.type}, Línea: {p.lineno}, Col: {p.lexpos}"
         # print(f"Error de sintaxis - Token: {p.type}, Línea: {p.lineno}, Col: {p.lexpos}")
         lstErrores.append(msg)
         parser.errok()
@@ -241,15 +253,15 @@ def p_error(p):
 parser = yacc.yacc()
 lstErrores = list()
 
+
 def validaRegla(s):
-  lstErrores.clear()
-  print(lstErrores) 
-  result = parser.parse(s)
-  logs = open('logs.txt','a')
-  logs.write(s+' '+str(datetime.now())+'\n')
-  return result
-  
-  
+    lstErrores.clear()
+    print(lstErrores)
+    result = parser.parse(s)
+    logs = open('logs.txt', 'a')
+    logs.write(s+' '+str(datetime.now())+'\n')
+    return result
+
 
 # while True:
 #     try:
@@ -261,6 +273,3 @@ def validaRegla(s):
 #         continue
 #     validaRegla(s)
 #     print(lstErrores)
-
-    
-
